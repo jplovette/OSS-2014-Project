9d6520150b2fc441bbfd61c469e2df59bd14b6f3
tonychangmsu <tony.chang@msu.montana.edu>
2014-08-04 12:45:44 -0400

    fixed hello_world.py

--- Scripts/hello_world.py
+++ Scripts/hello_world.py
@@ -1,5 +1,5 @@
 import numpy as np
 from matplotlib import pyplot as plt
 print('hello world')
-plt.plot(np.random(0,1,10))
+plt.plot(np.random.random(0,1,10)) #generate some random normal data
 plt.show()
