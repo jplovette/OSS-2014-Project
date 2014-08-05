746d4e0109e5a8a7519870d53db1aaa8977557a0
tonychangmsu <tony.chang@msu.montana.edu>
2014-08-05 06:39:51 -0400

    added tile preprocessing and data log

---
+++ Scripts/shape_extract.py
@@ -0,0 +1,10 @@
+#shape_extract
+#author: Tony Chang
+#abstract: This script takes the HUC shapefiles and extracts underlying rasters for analysis
+#date: 08.05.2014
+#organization: OSS 2014 
+#location: RENCI, Chapel Hill, NC
+#written for Python 3.3
+
+import numpy as np
+from matplotlib import pyplot as plt
